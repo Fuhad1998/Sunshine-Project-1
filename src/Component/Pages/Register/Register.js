79ce242a2c739c1b72c975711f0a562310584317
fuhad1998 <fuhadrare444@gmail.com>
2022-03-29 01:06:10 +0600

    styled login component

--- src/Component/Pages/Register/Register.js
+++ src/Component/Pages/Register/Register.js
@@ -1,11 +1,54 @@
-import React from 'react';
+import React from "react";
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
 
 const Register = () => {
-    return (
-        <div>
-            <h1>This is register</h1>
-        </div>
-    );
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => {
+    console.log(data.name);
+    console.log(data);
+    alert("Register successful");
+  };
+  return (
+    <div className="container  login-container my-5 shadow-lg">
+      <h1>Please Register</h1>
+      <form className="login-container" onSubmit={handleSubmit(onSubmit)}>
+        <input
+          className="input-btn p-2 rounded"
+          {...register("name")}
+          placeholder="Write your name"
+        />
+        <br />
+        <input
+          className="input-btn p-2 rounded"
+          {...register("email")}
+          placeholder="Write your email"
+        />
+        <br />
+        <input
+          className="input-btn p-2 rounded"
+          type="password"
+          {...register("password")}
+          placeholder="Write your password"
+        />
+        <br />
+        <input
+          className="btn-submit input-btn btn-primary fs-5 rounded"
+          type="submit"
+          value="Register"
+        />
+        <br />
+        <Link className="" to="/login">
+          <button
+            className="border-0 bg-success bg-opacity-25"
+            style={{ fontWeight: 600 }}
+          >
+            ALREADY REGISTERED? PLEASE LOG-IN
+          </button>
+        </Link>
+      </form>
+    </div>
+  );
 };
 
 export default Register;
